4acae082af9d9cb44cb73a1dd807ce848fdc394b
chengongwen <chengongwen@users.noreply.github.com>
2016-02-19 16:19:14 +0800

    Update AppDelegate.h

--- 菜单标题/下拉框/AJComboBoxDemo/AJComboBoxDemo/AppDelegate.h
+++ 菜单标题/下拉框/AJComboBoxDemo/AJComboBoxDemo/AppDelegate.h
@@ -1,10 +1,13 @@
 //
 //  AppDelegate.h
 //  AJComboBoxDemo
+// 
+//  组件集成
 //
 //  Created by yuanshanit on 15/3/25.
 //  Copyright (c) 2015年 元善科技. All rights reserved.
 //
+
 
 #import <UIKit/UIKit.h>
 
